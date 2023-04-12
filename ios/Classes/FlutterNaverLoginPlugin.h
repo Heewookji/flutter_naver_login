835e3d096d9d9151789dd8044e067e476304b1a7
heewookji <jhu2269@gmail.com>
2023-04-13 03:52:32 +0900

    chore: remove import

--- ios/Classes/FlutterNaverLoginPlugin.h
+++ ios/Classes/FlutterNaverLoginPlugin.h
@@ -1,14 +1,5 @@
 #import <Flutter/Flutter.h>
 
-#if __has_include(<NaverThirdPartyLogin/NaverThirdPartyLogin.h>)
-#import <NaverThirdPartyLogin/NaverThirdPartyLogin.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "NaverThirdPartyLogin.h"
-#endif
-
 @interface FlutterNaverLoginPlugin : NSObject<FlutterPlugin>
 {
     NaverThirdPartyLoginConnection *_thirdPartyLoginConn;
