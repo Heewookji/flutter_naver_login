5e5a5f77d3c2e0e32b713a9e3479f9d8696c7d9e
heewookji <jhu2269@gmail.com>
2023-04-13 02:23:30 +0900

    fix: ios import

--- ios/Classes/FlutterNaverLoginPlugin.h
+++ ios/Classes/FlutterNaverLoginPlugin.h
@@ -1,5 +1,13 @@
 #import <Flutter/Flutter.h>
+#import
+#if __has_include(<NaverThirdPartyLogin/NaverThirdPartyLogin.h>)
 #import <NaverThirdPartyLogin/NaverThirdPartyLogin.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "NaverThirdPartyLogin.h"
+#endif
 
 @interface FlutterNaverLoginPlugin : NSObject<FlutterPlugin>
 {
